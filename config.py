13a224d20fe0ed0b1ee46284f0bf342db31ab0b0
Yeikyu <koloradojdbck@hotmail.com>
2024-06-26 10:20:53 -0500

    configuracion para ronald y javier 	modified:   __pycache__/config.cpython-38.pyc 	new file:   app/__pycache__/__init__.cpython-38.pyc 	new file:   app/__pycache__/models.cpython-38.pyc 	new file:   app/__pycache__/views.cpython-38.pyc 	modified:   config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 
 class Config:
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///example.db'  # Conexión SQLite para desarrollo local
-    SQLALCHEMY_DATABASE_URI = 'mariadb+mariadbconnector://root:root@127.0.0.1/pruebamvc2'
+    #SQLALCHEMY_DATABASE_URI = 'mariadb+mariadbconnector://root:root@127.0.0.1/pruebamvc2'
+    SQLALCHEMY_DATABASE_URI = 'mariadb+mariadbconnector://root:root@127.0.0.1:3307/sgb_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'mysecretkey'  # Clave secreta para formularios
